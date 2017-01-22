de2c7aad4c47bb8c86dc0ef9d319a45b55bbc60e
andynhi <nhi.atn@gmail.com>
2017-01-21 17:11:01 -0800

    add keyframe animation to jumbotron

--- app/js/components/Jumbotron.js
+++ app/js/components/Jumbotron.js
@@ -1,10 +1,20 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const slideIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 
 const Jumbotron = styled.div`
   border-radius: 3px;
   background: rgba(3, 0, 0, 0.7);
   height: 800px;
   padding: 10px;
+  animation: ${slideIn} ease-in 1 .5s;
 `;
 
 export default Jumbotron;
